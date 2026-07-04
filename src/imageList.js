66897df647919b367e66e0b433d66e6065fc1d9b
Nguyenvanan <nguyenvanan13122006@gmail.com>
2026-07-04 10:59:11 +0700

    Fix duong dan anh tuong doi cho GitHub Pages

--- src/imageList.js
+++ src/imageList.js
@@ -1,17 +1,19 @@
+
+// file: imageList.js
 const photoUrls = [
-  "/images/planet1.jpg",
-  "/images/planet2.jpg",
-  "/images/planet3.jpg",
-  "/images/planet4.jpg",
-  "/images/planet5.jpg",
-  "/images/planet6.jpg",
-  "/images/planet7.jpg",
-  "/images/planet8.jpg",
-  "/images/planet9.jpg",
-  "/images/planet10.jpg",
-  "/images/planet11.jpg",
-  "/images/planet12.jpg",
-  "/images/planet13.jpg"
+  "images/planet1.jpg",
+  "images/planet2.jpg",
+  "images/planet3.jpg",
+  "images/planet4.jpg",
+  "images/planet5.jpg",
+  "images/planet6.jpg",
+  "images/planet7.jpg",
+  "images/planet8.jpg",
+  "images/planet9.jpg",
+  "images/planet10.jpg",
+  "images/planet11.jpg",
+  "images/planet12.jpg",
+  "images/planet13.jpg"
 ];
 
 export default photoUrls;
